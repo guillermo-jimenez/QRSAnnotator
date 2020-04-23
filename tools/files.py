db34120e252ef783d9ed31f9f581f16d4ecddd9d
Guillermo Jimenez-Perez <guillermo.jimenez@upf.edu>
2020-04-23 16:41:49 +0200

    Updated functions from app.py

--- tools/files.py
+++ tools/files.py
@@ -1,17 +1,15 @@
 import os
 import os.path
+import glob
 import numpy as np
 
-def list_files(path):
+def list_files(path: str, extension: str = '') -> list:
     opts = []
-
-    # r=root, d=directories, f = files
-    for r, di, f in os.walk(path):
-        if r == path:
-            for file in f:
-                if file.endswith('.txt'):
-                    opts.append({'label': file[:-4], 'value': os.path.join(r, file)})
-
+    for file in glob.glob(os.path.join(path,'*'+extension)):
+        if os.path.isfile(file):
+            fname, ext = os.path.splitext(file)
+            root, fname = os.path.split(fname)
+            opts.append({'label': fname, 'value': os.path.join(file)})
     return opts
 
 def sorter(total_items):
@@ -27,31 +25,27 @@ def conditional_makedir(path):
         import pathlib
         p = pathlib.Path(path)
         p.parent.mkdir(parents=True, exist_ok=True)
-
     except:
         list_path = os.path.normpath(path).split(os.sep)[1:]
-        t = '/'
-
+        t = os.sep
         for p in list_path:
             t = os.path.join(t,p)
-            
             if not os.path.exists(t):
                 os.makedirs(t)
 
 def save_fiducials(dictionary, path):
     with open(path, 'w') as f:
         for key in dictionary.keys():
-            f.write("%s,%s\n"%(key,str(list(dictionary[key])).replace('[','').replace(']','')))
+            if len(dictionary[key]) != 0:
+                f.write("%s,%s\n"%(key,str(dictionary[key]).replace('[','').replace(']','')))
 
 # Data loader to un-clutter code    
 def load_data(file):
     dic = dict()
-
     with open(file) as f:
         text = list(f)
-
     for line in text:
-        line = line.replace(' ','').replace('\n','').replace(',,','')
+        line = line.replace(' ','').replace('\n','').replace(',,','').replace('[','').replace(']','')
         if line[-1] == ',': line = line[:-1]
         head = line.split(',')[0]
         tail = line.split(',')[1:]
@@ -61,6 +55,5 @@ def load_data(file):
             tail = np.asarray(tail).astype(int).tolist()
         
         dic[head] = tail
-
     return dic
 
